cd901dee836afe5cc9fb8cc9d5a33d050449ccb7
Nimishk21 <Nimish@21>
2020-05-25 00:51:47 -0500

    commit

---
+++ get highest answer rate question.sql
@@ -0,0 +1,7 @@
+select question_id as survey_log
+from(
+select question_id,(sum(case when action='answer' then 1 else 0 end)/sum(case when action='show' then 1 else 0 end)) as rate
+from survey_log
+group by question_id) a
+order by rate desc
+limit 1
