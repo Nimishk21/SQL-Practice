373afbb93115301e635134e1e23141b21cab4b33
Nimish Kanani <nimishk21@gmail.com>
2020-06-10 17:58:06 -0500

    Update Trips and Users.sql

--- Trips and Users.sql
+++ Trips and Users.sql
@@ -1,9 +1,12 @@
-select request_at as Day,round(((sum(case when status like 'cancelled%' Then 1.00 else 0.00 end)) / count(*)),2) as 'Cancellation Rate'
-from trips t
-join users u on t.client_id=u.users_id
-where banned='No' and request_at BETWEEN '2013-10-01' AND '2013-10-03'
-group by request_at
+with cte as(
+select *
+from trips
+where client_id not in(select users_id from users where banned='Yes')
+      and driver_id  not in(select users_id from users where banned='Yes')
+      and request_at between '2013-10-01' and '2013-10-03')
 
-
-
-
+select request_at as Day,round(sum(case when status like 'Cancelled%' then 1 else 0 end)/count(*),2) as 'Cancellation rate'
+from cte
+group by request_at;
+    
+    
