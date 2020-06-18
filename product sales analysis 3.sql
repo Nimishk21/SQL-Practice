c079200d7332c7e720d90ce28b304c632847d165
Nimish Kanani <nimishk21@gmail.com>
2020-06-17 23:57:27 -0500

    Update product sales analysis 3.sql

--- product sales analysis 3.sql
+++ product sales analysis 3.sql
@@ -4,3 +4,11 @@ WHERE (product_id, year) IN (
 SELECT product_id, MIN(year) as year
 FROM Sales
 GROUP BY product_id)
+
+
+select product_id, year as first_year, quantity, price from
+(
+select 
+    product_id, year, quantity, price, dense_rank() over (partition by product_id order by year) as rank1
+from Sales) tmp
+where rank1 = 1;
