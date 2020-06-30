16cf8519d503916c7ebd89e38247bd07fa2ec39d
Nimish Kanani <nimishk21@gmail.com>
2020-06-29 23:05:39 -0500

    Create Capital Gain-loss.sql

---
+++ Capital Gain-loss.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select stock_name,sum(case when operation='sell' then price 
+                       when operation='buy' then (price*-1) end) as capital_gain_loss
+from stocks
+group by stock_name
+
+# Write your MySQL query statement below
+select stock_name,sum(case when operation='sell' then price else 0 end)-sum(case when operation='buy' then price else 0 end) as capital_gain_loss
+from stocks
+group by stock_name
